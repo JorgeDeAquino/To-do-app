1555e614bd34e60ce1e9a07a2f5522b01e4d694d
Jorge Alexandre <jorgeaamsantos@gmail.com>
2022-11-21 17:50:05 -0300

    add removeall.js, iniciado função para excluir todos

---
+++ assets/js/removeall.js
@@ -0,0 +1,11 @@
+const removeAll = document.querySelector(".btn__limpar__campos");
+const itensUl = document.querySelectorAll(".circulo");
+
+function removerTodos() {
+    console.log(itensUl);
+    removeAll.addEventListener('click', () => {
+        //transformar o html colection em array e excluir item por item
+    })
+}
+
+// removerTodos()
